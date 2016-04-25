baf53918052932c3d4f312b864a14953876ead68
Bill Spencer <badgley@gmail.com>
2016-04-25 14:04:57 -0400

    changed main and winnings print out

--- src/SlotMachine1/machineMain.java
+++ src/SlotMachine1/machineMain.java
@@ -7,33 +7,38 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
 public class machineMain {
-static Scanner sc = new Scanner(System.in);
+	static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		File slot = new File("slot.wav");
-		
+
 		System.out.println("Welcome to the BDJS SLOT MACHINE!");
 		System.out.println("You have 1000 credits!");
-		
+
 		String keepPlaying = "y";
-		while(keepPlaying.equalsIgnoreCase("y")){
-		System.out.print("How much would you like to bet?:");
-		// Validation for player input...between 1 and money left in bank
-		int input = validator.getValidInt(1, player.getBank());
-		
-		player.setBet(input);
-		//subtract bet from bank
-		player.setBank(player.getBank()-player.getBet());
-		PlaySound(slot);
-		slots.checkWin();
-		
-		System.out.print("Would you like to try your luck again (y/n): ");
-		keepPlaying = validator.readYorN("y", "n");
-		}//end while to keep going
+		while (keepPlaying.equalsIgnoreCase("y")) {
+			System.out.print("How much would you like to bet?:");
+			// Validation for player input...between 1 and money left in bank
+			int input = validator.getValidInt(1, player.getBank());
+
+			player.setBet(input);
+			// subtract bet from bank
+			player.setBank(player.getBank() - player.getBet());
+			PlaySound(slot);
+			slots.checkWin();
+			if (player.getBank() == 0) {
+				System.out.println("Take your BROKE ass home, bro!!");
+				return;
+			}
+			System.out.print("Would you like to try your luck again (y/n): ");
+			keepPlaying = validator.readYorN("y", "n");
+		} // end while to keep going
+
 		System.out.println("Thanks for playing.");
 		System.out.println("Please cash out at the front desk.");
 	}
 
-//	PlaySound method plays clip from file (taken from input)
+	// PlaySound method plays clip from file (taken from input)
 	public static void PlaySound(File Sound) {
 		try {
 			Clip clip = AudioSystem.getClip();
--- src/SlotMachine1/slots.java
+++ src/SlotMachine1/slots.java
@@ -7,46 +7,53 @@ public class slots {
 	private static int wheel2;
 	private static int wheel3;
 	private static int gameWin;
-	
-	public static int randomGen(){
-		Random r = new Random ();
-		int random  = r.nextInt(5)+1;
+	private static int winnings;
+
+	public static int randomGen() {
+		Random r = new Random();
+		int random = r.nextInt(5) + 1;
 		return random;
 	}
-	
-	public static void checkWin (){
-		wheel1= randomGen ();
-		wheel2= randomGen ();
-		wheel3= randomGen ();
-		//printBoard
+
+	public static void checkWin() {
+		wheel1 = randomGen();
+		wheel2 = randomGen();
+		wheel3 = randomGen();
+		// printBoard
 		printBoard();
-		//check for win and send to pay
-		if (wheel1 == wheel2 && wheel2 == wheel3){
-			gameWin= wheel1;
+		// check for win and send to pay
+		if (wheel1 == wheel2 && wheel2 == wheel3) {
+			gameWin = wheel1;
 			payOut();
-			System.out.println("You're a Winner!!!!" + "\n");
-		}else {
+			System.out.println("You're a Winner!!!!");
+			System.out.println("You won + " + winnings);
+		} else {
 			System.out.println("You lose!" + "\n");
 		}
 		System.out.println("Your bank: $" + player.getBank());
 	}
-	
-	public static void payOut (){
-		switch (gameWin){
-		case 1: 
-				player.setBank((player.getBank()+(player.getBet()*2)));
-		case 2: 
-				player.setBank((player.getBank()+(player.getBet()*3)));
-		case 3: 
-				player.setBank((player.getBank()+(player.getBet()*4)));
-		case 4: 
-				player.setBank((player.getBank()+(player.getBet()*5)));
-		case 5: 
-				player.setBank((player.getBank()+(player.getBet()*10)));
+
+	public static void payOut() {
+		switch (gameWin) {
+		case 1:
+			winnings = player.getBank() * 2;
+			player.setBank((player.getBank() + winnings));
+		case 2:
+			winnings = player.getBank() * 3;
+			player.setBank((player.getBank() + winnings));
+		case 3:
+			winnings = player.getBank() * 4;
+			player.setBank((player.getBank() + winnings));
+		case 4:
+			winnings = player.getBank() * 5;
+			player.setBank((player.getBank() + winnings));
+		case 5:
+			winnings = player.getBank() * 10;
+			player.setBank((player.getBank() + winnings));
 		}
 	}
-	
-	public static void printBoard(){
+
+	public static void printBoard() {
 		System.out.println(wheel1 + " | " + wheel2 + " | " + wheel3);
 	}
 }
