ff96933390b4a3b609210a5ffee4a5581bc9afb9
Jason Scott <becomingcoder78@gmail.com>
2016-04-25 11:16:53 -0400

    Updated validation for player class and main

--- src/SlotMachine1/validator.java
+++ src/SlotMachine1/validator.java
@@ -1,27 +1,26 @@
 package SlotMachine1;
 
-import java.util.Scanner;
 
 public class validator {
 	
-	static Scanner sc = new Scanner(System.in);
+
 	int playerbank = 50;
 	
 	// Validates that input is an integer value
 	public static int readValidInt() {
-		while (!sc.hasNextInt()) {
-			sc.nextLine();
+		while (!machineMain.sc.hasNextInt()) {
+			machineMain.sc.nextLine();
 			System.out.print("Please enter a valid number: ");
 		}
-		int x = sc.nextInt();
-		sc.nextLine();
+		int x = machineMain.sc.nextInt();
+		machineMain.sc.nextLine();
 		return x;
 	}
 	// Validates that input is within the range you set.  Will need to be called in main.
 	public static int getValidInt(int min, int max) {
 		int i = readValidInt();
 		while (i < min || i > max) {
-			System.out.print("Bruh, you know you only have " + playerbank + " dollars:");  // this can be changed to needed values
+			System.out.print("Bruh, you know you only have " + player.getBank() + " dollars:");  // this can be changed to needed values
 			i = readValidInt();
 		}
 		return i;
