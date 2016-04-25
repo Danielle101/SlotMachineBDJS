5a28443444da1242a9f62e378d6a43fb119a54ff
Bill Spencer <badgley@gmail.com>
2016-04-25 11:06:20 -0400

    added sound file and PlaySound method to main

--- src/SlotMachine1/machineMain.java
+++ src/SlotMachine1/machineMain.java
@@ -1,15 +1,34 @@
 package SlotMachine1;
-import java.util.*;
+
+import java.io.File;
+import java.util.Scanner;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 public class machineMain {
 
 	public static void main(String[] args) {
+		File slot = new File("slot.wav");
+		// PlaySound(slot);
 		Scanner sc = new Scanner(System.in);
 		// TODO Auto-generated method stub
-System.out.println("Welcome to the BDJS SLOT MACHINE!");
-System.out.println("You have 1000 credits!");
-System.out.println("How much would you like to bet?:");
-int input = 0;
-input = sc.nextInt();
+		System.out.println("Welcome to the BDJS SLOT MACHINE!");
+		System.out.println("You have 1000 credits!");
+		System.out.println("How much would you like to bet?:");
+		int input = 0;
+		input = sc.nextInt();
+	}
+
+//	PlaySound method plays clip from file (taken from input)
+	public static void PlaySound(File Sound) {
+		try {
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(Sound));
+			clip.start();
+		} catch (Exception e) {
+
+		}
 	}
 
 }
