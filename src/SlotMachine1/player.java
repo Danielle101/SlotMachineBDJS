f61f167ce09fe80e882c4a6932c1f9de3d19370f
Bill Spencer <badgley@gmail.com>
2016-04-25 10:54:49 -0400

    setters and getters added for player class

--- src/SlotMachine1/player.java
+++ src/SlotMachine1/player.java
@@ -1,5 +1,20 @@
 package SlotMachine1;
 
 public class player {
+	static int bank = 1000;
+	static int bet;
+	public static int getBank() {
+		return bank;
+	}
+	public static int getBet() {
+		return bet;
+	}
+	public static void setBank(int bank) {
+		player.bank = bank;
+	}
+	public static void setBet(int bet) {
+		player.bet = bet;
+	}
+	
 
 }
