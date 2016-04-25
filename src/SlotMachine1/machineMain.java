1844730592de9fc53c72870ff94b15480e4c9324
DanielleHall <halldan4@yahoo.com>
2016-04-25 11:13:17 -0400

    static scanner in main

--- src/SlotMachine1/machineMain.java
+++ src/SlotMachine1/machineMain.java
@@ -7,17 +7,16 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
 public class machineMain {
-
+static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 		File slot = new File("slot.wav");
 		// PlaySound(slot);
-		Scanner sc = new Scanner(System.in);
-		// TODO Auto-generated method stub
+		
 		System.out.println("Welcome to the BDJS SLOT MACHINE!");
 		System.out.println("You have 1000 credits!");
 		System.out.println("How much would you like to bet?:");
-		int input = 0;
-		input = sc.nextInt();
+		int input = sc.nextInt();
+	 player.setBet(input);
 	}
 
 //	PlaySound method plays clip from file (taken from input)
