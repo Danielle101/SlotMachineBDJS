25be2fec0525555c65f7c903af75be99891c4f9e
DanielleHall <halldan4@yahoo.com>
2016-04-25 10:59:36 -0400

    added scanner

--- src/SlotMachine1/machineMain.java
+++ src/SlotMachine1/machineMain.java
@@ -3,10 +3,13 @@ import java.util.*;
 public class machineMain {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		// TODO Auto-generated method stub
 System.out.println("Welcome to the BDJS SLOT MACHINE!");
 System.out.println("You have 1000 credits!");
 System.out.println("How much would you like to bet?:");
+int input = 0;
+input = sc.nextInt();
 	}
 
 }
