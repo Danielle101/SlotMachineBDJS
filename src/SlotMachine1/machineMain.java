6d62e81d8daafeea42567cfee65af5a70c279329
DanielleHall <halldan4@yahoo.com>
2016-04-25 10:45:45 -0400

    added welcome prompt

--- src/SlotMachine1/machineMain.java
+++ src/SlotMachine1/machineMain.java
@@ -1,10 +1,12 @@
 package SlotMachine1;
-
+import java.util.*;
 public class machineMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("Welcome to the BDJS SLOT MACHINE!");
+System.out.println("You have 1000 credits!");
+System.out.println("How much would you like to bet?:");
 	}
 
 }
